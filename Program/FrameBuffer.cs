e0ddc7cc7397e9b3c089e2fc77860017968b71fe
Darren R. Starr <darren@nocturnal.no>
2019-02-12 16:25:38 +0100

    Typo

--- Program/FrameBuffer.cs
+++ Program/FrameBuffer.cs
@@ -96,7 +96,7 @@ namespace Program
         public FrameBuffer()
         {
             var fb0Handle = Open("/dev/i2c-1", OPEN_READ_WRITE);
-            if(fb0Handle < -1) {
+            if(fb0Handle < 0) {
                 Console.WriteLine($"Failed to open /dev/fb0 for read and write {fb0Handle}");
 
                 Console.WriteLine("Error code is {0}", Marshal.GetLastWin32Error());
